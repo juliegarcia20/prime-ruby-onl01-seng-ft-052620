b51375acb6cb0580dc0e9d17ad4aae1502ce0345
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-03 04:53:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,12 @@
-
+def is_prime?(num)
+    flag = true
+    for x in 2..num-1
+        if num % x == 0
+            flag = false
+        end
+    end
+    return flag
+end
 
  # all_odd = true
 #[1,2,3].each do |number|
