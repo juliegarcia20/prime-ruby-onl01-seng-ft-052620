1d05e958dc08b37c4c53bfa27a4b5e25aa425ad5
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-03 04:49:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+def prime?(n)
+  n = 2
+if (n).nonzero? 
+  return true
+else
+  return false
+  end 
+end
+
+
+
+ # all_odd = true
+#[1,2,3].each do |number|
+  #if number.even? # Will evaluate to false for 1, true for 2, false for 3
+    #all_odd = false
+ # end
+#end
+
+#def long_planeteer_calls(array)
+ # i = 0 
+  #if array.any? {|i| i.length > 4} 
+   #return true
+  #else 
+  #  return false 
+#end
